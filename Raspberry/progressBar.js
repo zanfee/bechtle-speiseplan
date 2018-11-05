97125cf31ece42d23ea2e977a61f4134ff0058d4
Jan <jan.froehlich.janf@gmail.com>
2018-11-05 16:36:56 +0100

    Todo Progressbar

--- Raspberry/progressBar.js
+++ Raspberry/progressBar.js
@@ -4,9 +4,14 @@ var bar = document.getElementById('progress-bar');
 function move() {
     if (width < 100) {
         width += 0.5;
-        bar.style.width = width + '%';
-        window.requestAnimationFrame(move);
-    } 
+    }
+    else {
+        width = 0;
+        // color animation green → grey ~2s
+        // scroll to next element
+    }
+    bar.style.width = width + '%';
+    window.requestAnimationFrame(move);
 }
 
 window.requestAnimationFrame(move);
