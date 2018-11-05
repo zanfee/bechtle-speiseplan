133d5cf53c4521e9ec7b2e022a12c7893690120d
Jan <jan.froehlich.janf@gmail.com>
2018-11-05 16:45:29 +0100

    Cleanup

--- Raspberry/progressBar.js
+++ Raspberry/progressBar.js
@@ -7,8 +7,10 @@ function move() {
     }
     else {
         width = 0;
+        /* --- 05.11.2018-jf ---
         // color animation green → grey ~2s
         // scroll to next element
+        */
     }
     bar.style.width = width + '%';
     window.requestAnimationFrame(move);
