e80a68e16e1a6ff564d00aa8c663f0cb18b38ecf
Jan <jan.froehlich.janf@gmail.com>
2018-11-12 09:58:27 +0100

    Working Menu Button

--- Raspberry/page.js
+++ Raspberry/page.js
@@ -5,3 +5,10 @@ function toggleMenu() {
     mover.className = mover.className === 'go-left'? 'go-right': 'go-left';
 }
 trigger.addEventListener('click', toggleMenu);
+trigger.addEventListener("keyup", function(event) {
+
+  if (event.keyCode === 13 | event.keyCode === 32) {
+
+    trigger.click();
+  }
+});
