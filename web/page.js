f11a739e394836d329e06d7892199bf7c2ef8ebe
Jan <jan.froehlich.janf@gmail.com>
2018-11-13 10:59:59 +0100

    Implement Admin Page

--- web/page.js
+++ web/page.js
@@ -4,31 +4,36 @@ var navButton = document.getElementById('nav-button');
 var modules = document.getElementById('modules').childNodes;
 var settings = document.getElementById('settings').childNodes;
 
+var content = document.getElementById('content');
+
 function toggleMenu() {
     navBody.className = navBody.className === 'go-left' ? 'go-right' : 'go-left';
 }
 
-function expand(element) {
-  if (element === 0) {
-    for (i=0; i < modules.length; i++) {
-      if (modules[i].className === 'hidden') {
-        modules[i].className = 'shown';
-      }
-      else if (modules[i].className === 'shown') {
-        modules[i].className = 'hidden';
-      }
+function expandModules() {
+  for (i=0; i < modules.length; i++) {
+    if (modules[i].className === 'hidden') {
+      modules[i].className = 'shown';
+    }
+    else if (modules[i].className === 'shown') {
+      modules[i].className = 'hidden';
     }
   }
-  else if (element === 1) {
-    for (i=0; i < settings.length; i++) {
-      if (settings[i].className === 'hidden') {
-        settings[i].className = 'shown';
-      }
-      else if (settings[i].className === 'shown') {
-        settings[i].className = 'hidden';
-      }
+}
+
+function expandSettings() {
+  for (i=0; i < settings.length; i++) {
+    if (settings[i].className === 'hidden') {
+      settings[i].className = 'shown';
+    }
+    else if (settings[i].className === 'shown') {
+      settings[i].className = 'hidden';
     }
   }
+}
+
+function loadAdmin() {
+  content.innerHTML = '<object type="text/html" data="/admin/adminTool.html"></object>';
 }
 
 navButton.addEventListener('click', toggleMenu);
