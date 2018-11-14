b6f8267192dfaed50a6f3f01fa9a899882a35317
Patrick Hollweck <patrick-hollweck@gmx.de>
2018-11-14 21:31:07 +0100

    Refactor navigation

--- web/navigation.js
+++ web/navigation.js
@@ -6,18 +6,15 @@ var navOpen = document.getElementById('nav-open');
 var modules = document.getElementById('modules').childNodes;
 var settings = document.getElementById('settings').childNodes;
 
-//content.innerHTML = '<object type="text/html" data="./Speiseplan/viewCarousel/carousel.html" class="fullscreen"></object>';
+var date = document.getElementById("date");
+var clock = document.getElementById("clock");
 
 /* Progress Bar*/
 var barGradient = -400;
 var progressbar = document.getElementById('progress');
 
-/* Date */
-var time, hours, minutes, seconds, weekDay, day, month, year;
-
 /* Content */
 var content = document.getElementById('content');
-var page;
 
 /* Navigation */
 function toggleMenu() {
@@ -36,52 +33,52 @@ function closeMenu() {
   navOpen.className = 'hidden';
 }
 
-function expandModules() {
-  for (i=0; i < modules.length; i++) {
-    if (modules[i].className === 'hidden') {
-      modules[i].className = 'shown';
-    }
-    else if (modules[i].className === 'shown') {
-      modules[i].className = 'hidden';
-    }
-  }
-}
+function expandList(id) {
+  var nodes = document.getElementById(id).childNodes;
 
-function expandSettings() {
-  for (i=0; i < settings.length; i++) {
-    if (settings[i].className === 'hidden') {
-      settings[i].className = 'shown';
+  for (var i=0; i < settings.length; i++) {
+    var current = nodes[i];
+
+    if (current.classList.contains('hidden')) {
+      current.classList.add('shown');
+      current.classList.remove('hidden');
     }
-    else if (settings[i].className === 'shown') {
-      settings[i].className = 'hidden';
+    else if (current.classList.contains('shown')) {
+      current.classList.add('hidden');
+      current.classList.remove('shown');
     }
   }
 }
 
-function loadAdmin() {
-  content.innerHTML = '<iframe type="text/html" id="page-object" class="fullscreen" frameborder="0" src="./admin/adminTool.html"></iframe>';
-}
-
-function loadWeek() {
-  content.innerHTML = '<iframe type="text/html" id="page-object" class="fullscreen" frameborder="0" src="./Speiseplan/viewWeek/week.html"></iframe>';
-}
-
-function loadDay() {
-  content.innerHTML = '<iframe type="text/html" id="page-object" class="fullscreen" frameborder="0" src="./Speiseplan/viewDay/day.html"></iframe>';
-}
-
-function loadCarousel() {
-  content.innerHTML = '<iframe type="text/html" id="page-object" class="fullscreen" frameborder="0" src="./Speiseplan/viewCarousel/carousel.html"></iframe>';
-}
+var KEYCODE_SPACE = 32;
+var KEYCODE_ENTER = 13;
 
 navButton.addEventListener('click', toggleMenu);
 navButton.addEventListener("keyup", function(event) {
-
-  if (event.keyCode === 13 | event.keyCode === 32) {
-
+  if (event.keyCode === KEYCODE_ENTER || event.keyCode === KEYCODE_SPACE) {
     navButton.click();
   }
 });
+
+function setContentPath(path) {
+  content.innerHTML = '<iframe type="text/html" id="page-object" class="fullscreen" frameborder="0" src="' + path + '"></iframe>'
+}
+
+function loadAdmin() {
+  setContentPath('./admin/adminTool.html')
+}
+
+function loadWeek() {
+  setContentPath('./Speiseplan/viewWeek/week.html');
+}
+
+function loadDay() {
+  setContentPath('./Speiseplan/viewDay/day.html"></iframe>');
+}
+
+function loadCarousel() {
+  setContentPath('./Speiseplan/viewCarousel/carousel.html"></iframe>');
+}
 
 loadCarousel();
 
@@ -105,42 +102,12 @@ function move() {
 window.requestAnimationFrame(move);
 
 /* Clock */
-function tick(){    
-  time = new Date();
-  hours = time.getHours();
-  minutes = time.getMinutes();
-  seconds = time.getSeconds();
-  weekDay = time.getDay();
-  day = time.getDate();
-  month = time.getMonth() + 1;
-  year = time.getFullYear();
+function tick() {
+  var currentTime = new Date();
+  clock.innerHTML = currentTime.toLocaleTimeString();
 
-  if (minutes < 10) {minutes = "0" + minutes}
-  if (seconds < 10) {seconds = "0" + seconds}
-  time = hours + ":" + minutes + ":" + seconds + " Uhr";
-  clock.innerHTML = time;
-
-  switch(weekDay){
-      case 1: weekDay = "Montag, ";
-          break;
-      case 2: weekDay = "Dienstag, ";
-          break;
-      case 3: weekDay = "Mittwoch, ";
-          break;
-      case 4: weekDay = "Donnerstag, ";
-          break;
-      case 5: weekDay = "Freitag, ";
-          break;
-      case 6: weekDay = "Samstag, ";
-          break;
-      case 7: weekDay = "Sonntag, ";
-          break;
-  }
-
-  if (month < 10) {month = "0" + month}
-  if (day < 10) {day = "0" + day}
-  getDate = weekDay.bold() + day + "." + month + "." + year;
-  date.innerHTML = getDate;
+  var weekDay = currentTime.toLocaleDateString("de-DE", { weekday: "long" });
+  date.innerHTML = weekDay.bold() + ", " + currentTime.toLocaleDateString("de-DE");
 
   window.setTimeout("tick();", 1000);
 }
