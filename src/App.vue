84bcbaaae5276bbea5a45b27962eb601ca3b4d7b
Patrick Hollweck <patrick-hollweck@gmx.de>
2018-11-18 01:46:11 +0100

    Import main files in vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,53 @@
 <template>
-  <div id="app">
-    <h1>Hello World</h1>
-  </div>
+<div>
+    <div class="nav-header">
+        <div class="header">
+            <div id="nav-button" tabindex="1">
+                <div class="bar"></div>
+                <div class="bar"></div>
+                <div class="bar"></div>
+            </div>
+
+            <h5 class="pagename"><strong>Bechtle</strong> Speiseplan</h5>
+            <h6 id="clock"></h6>
+            <h6 id="date"></h6>
+        </div>
+
+        <div id="progress"></div>
+    </div>
+
+    <nav id="nav-body" class="go-left">
+        <ul class="nav-admin unselectable">
+            <a class="menuPoint" onclick="loadAdmin()" style="color: darkred;">Bearbeiten</a>
+        </ul>
+        <ul class="nav-view unselectable">
+            <a class="menuPoint" onclick="loadWeek()">Wochenübersicht</a>
+            <a class="menuPoint" onclick="loadDay()">Tagesübersicht</a>
+            <a class="menuPoint" onclick="loadCarousel()">Karussell Ansicht</a>
+        </ul>
+        <ul class="nav-base unselectable">
+            <a onclick="expandList('modules')">Module</a>
+            <div class="menuPoint" id="modules">
+                <a class="indented">Speiseplan Karussell</a>
+                <a class="indented">Speiseplan Tag</a>
+                <a class="indented">Speiseplan Woche</a>
+                <a class="indented">Wetter</a>
+            </div>
+            <a onclick="expandList('settings')">Einstellungen</a>
+            <div class="menuPoint" id="settings">
+                    <a onclick="ThemeService.toggleCurrent();" class="indented">Nachtmodus</a>
+                    <a class="indented">Language</a>
+                    <a class="indented">Geschwindigkeit</a>
+                </div>
+            </ul>
+    </nav>
+
+    <div id="nav-open" class="unselectable hidden" onclick="closeMenu()" style="width:100%"></div>
+
+    <div id="content"></div>
+</div>
 </template>
+
+<script>
+export default {};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import Vue from "vue";
 import App from "./App.vue";
 
+import "../web/theme.js";
+
+import "../web/stylesheet.css";
+import "../web/navigation.css";
+
+import "../web/navigation.js";
+
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
 }).$mount("#app");
