02da6783d83e2239f86817f05e9c89d74f032c80
Pasquale Di Vincenzo <pasquale.divincenzo@outlook.de>
2018-11-30 16:23:17 +0100

    Added Forms for Admin Page

--- src/pages/AdminPage.vue
+++ src/pages/AdminPage.vue
@@ -1,5 +1,18 @@
 <template>
-
+<div class="page">
+    <div class="center">
+        <div class="container" id="calendar">
+            <div class="containerHeader">
+                <h1>Month</h1>
+            </div>
+        </div>
+        <div class="container" id="station">
+            <div class="containerHeader">
+                <h1>Station: one&sup1</h1>
+            </div>
+        </div>
+    </div>    
+  </div>  
 </template>
 
 <script>
@@ -7,5 +20,45 @@
 </script>
 
 <style scoped>
+.page{
+    float: left;
+    position: relative;
+    left: 50%;
+}
 
+.center{
+    width: 1050px;
+    float: left;
+    position: relative;
+    top: 100px;
+    left: -50%;
+}
+
+.container{
+    height: 500px;
+    width: 500px;
+    background-color: #505050;
+    border-radius: 20px;
+}
+
+#calendar{
+    float: left;
+}
+
+#station{
+    float: right;
+}
+
+.containerHeader{
+    height: 70px;
+    width: 100%;
+    border-radius: 20px;
+    background-color: #757575;
+}
+
+h1{
+    text-align: center;
+    margin: 0;
+    font-size: 50px;
+}
 </style>
