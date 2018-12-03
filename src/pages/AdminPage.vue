3c663b4764f2198c3ef10820cf6986ebf92e9d46
Jan <jan.froehlich.janf@gmail.com>
2018-12-03 10:26:02 +0100

    Hotfix

--- src/pages/AdminPage.vue
+++ src/pages/AdminPage.vue
@@ -8,57 +8,66 @@
         </div>
         <div class="container" id="station">
             <div class="containerHeader">
-                <h1>Station: one¹</h1>
+                <h1>Station: one</h1>
             </div>
         </div>
     </div>    
-  </div>  
+</div>  
 </template>
 
 <script>
-
+export default {
+  data() {
+    return {
+    };
+  },
+  methods: {
+  },
+  mounted() {
+  }
+};
 </script>
 
 <style scoped>
-.page{
-    float: left;
-    position: relative;
-    left: 50%;
+.page {
+  float: left;
+  position: relative;
+  left: 50%;
 }
 
-.center{
-    width: 1050px;
-    float: left;
-    position: relative;
-    top: 100px;
-    left: -50%;
+.center {
+  width: 1050px;
+  float: left;
+  position: relative;
+  top: 100px;
+  left: -50%;
 }
 
-.container{
-    height: 500px;
-    width: 500px;
-    background-color: #505050;
-    border-radius: 20px;
+.container {
+  height: 500px;
+  width: 500px;
+  background-color: #505050;
+  border-radius: 20px;
 }
 
-#calendar{
-    float: left;
+#calendar {
+  float: left;
 }
 
-#station{
-    float: right;
+#station {
+  float: right;
 }
 
-.containerHeader{
-    height: 70px;
-    width: 100%;
-    border-radius: 20px;
-    background-color: #757575;
+.containerHeader {
+  height: 70px;
+  width: 100%;
+  border-radius: 20px;
+  background-color: #757575;
 }
 
-h1{
-    text-align: center;
-    margin: 0;
-    font-size: 50px;
+h1 {
+  text-align: center;
+  margin: 0;
+  font-size: 50px;
 }
 </style>
