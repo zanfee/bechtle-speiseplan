2d4e7639b6f9805f0b15a9a520ebd609ffbb6c95
Jan <jan.froehlich.janf@gmail.com>
2018-12-19 16:25:53 +0100

    Wochenplan start

--- src/pages/WeekPage.vue
+++ src/pages/WeekPage.vue
@@ -1,7 +1,53 @@
 <template>
-<div>
-  <p style="text-align: center; font-family: Source Sans Pro; font-size: 16pt; font-weight: 700;">Wochenübersicht Speiseplan</p>
-</div>
+<table>
+  <tr>
+    <th>Montag</th>
+    <th>Dienstag</th>
+    <th>Mittwoch</th> 
+    <th>Donnerstag</th> 
+    <th>Freitag</th> 
+  </tr>
+  <tr>
+    
+    <td>Jill</td>
+    <td>94</td>
+  </tr>
+  <tr>
+    
+    <td>Eve</td>
+    <td>Jackson</td> 
+  </tr>
+  <tr>
+    
+    <td>Eve</td>
+    <td>Jackson</td> 
+  </tr>
+  <tr>
+    
+    <td>Eve</td>
+    <td>Jackson</td> 
+  </tr>
+</table>
+<!-- <div>
+  <div class="block station">
+    <h1 class="heading">Station</h1>
+  </div>
+  <div class="block weekday">
+    <h1 class="heading">Montag</h1>
+  </div>
+  <div class="block weekday">
+    <h1 class="heading">Dienstag</h1>
+  </div>
+  <div class="block weekday">
+    <h1 class="heading">Mittwoch</h1>
+  </div>
+  <div class="block weekday">
+    <h1 class="heading">Donnerstag</h1>
+  </div>
+  <div class="block weekday">
+    <h1 class="heading">Freitag</h1>
+  </div>
+</div> -->
 </template>
 
 <script>
@@ -9,4 +55,62 @@ export default {};
 </script>
 
 <style>
+table {
+  width: 100%;
+  /* table; */
+}
+
+th {
+  font-size: 3em;
+}
+
+.fit th {
+  font-size: 4vw;
+}
+
+td {
+  text-align: center;
+}
+
+.heading {
+  font-size: 1em;
+  text-align: center;
+}
+
+.fit .heading {
+  font-size: 1em;
+  text-align: center;
+}
+
+.block {
+  display: inline-block;
+  font-size: 3vw;
+  margin: 0;
+  font-size: 3.5em;
+}
+
+.weekday {
+  width: 326px;
+}
+
+.fit .weekday {
+  width: 17%;
+}
+
+.station {
+  width: 288px;
+  height: 100%;
+}
+
+.fit .station {
+  width: 15%;
+  height: 100%;
+}
+
+.title {
+  text-align: center;
+  margin: 0;
+  font-size: 3.5em;
+
+}
 </style>
