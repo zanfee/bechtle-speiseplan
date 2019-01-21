20c243810365eba941e32889c73c09671bd0c1ac
Patrick Hollweck <patrick-hollweck@gmx.de>
2019-01-21 21:22:47 +0100

    Split TvTile into multiple components

---
+++ src/components/Tiles/Food/FoodTileTag.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <img :src="getImgUrl(tag)">
+    <p class="tagName">{{ getTagName(tag) }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    tag: String
+  },
+  methods: {
+    getImgUrl(img) {
+      var images = require.context("../../../assets/img/", false, /\.svg$/);
+      return images("./" + img + ".svg");
+    },
+    getTagName(tag) {
+      return {
+        vegan: "Vegan",
+        vegetarisch: "Vegetarisch",
+        renewable: "Nachhaltige Fischerei",
+        wellfit: "Well Fit"
+      }[tag];
+    }
+  }
+};
+</script>
+
+<style scoped>
+.tagName {
+  text-align: center;
+  margin: 0;
+  font-size: 0.8vw;
+  font-weight: bold;
+}
+
+img {
+  width: 80%;
+  margin: 0 auto;
+  display: block;
+  padding: 10%;
+}
+</style>
--- src/pages/CarouselPage.vue
+++ src/pages/CarouselPage.vue
@@ -1,17 +1,19 @@
 <template>
 <div id="page">
   <div v-for="(item, index) in items" v-bind:key="index">
-    <TvTile :display="index"
+    <FoodTile
+      :display="index"
       :price="item[0]"
       :name="item[1]"
       :description="item[2]"
-    :tags="item[3]"></TvTile>
+      :tags="item[3]"
+    ></FoodTile>
   </div>
 </div>
 </template>
 
 <script>
-import TvTile from "../components/TvTile";
+import FoodTile from "../components/Tiles/Food/FoodTile";
 
 export default {
   data() {
@@ -25,10 +27,7 @@ export default {
     }
   },
   components: {
-    TvTile
+    FoodTile
   }
 };
 </script>
-
-<style>
-</style>
