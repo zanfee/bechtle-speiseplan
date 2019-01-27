84dc7c5031457a731536b8b0f38f47c8f9d9335b
Patrick Hollweck <patrick-hollweck@gmx.de>
2019-01-27 22:33:36 +0100

    Set cors header

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,20 +1,40 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
 
-const admin = require('firebase-admin');
+const cors = require("cors")({
+  origin: true
+});
+
+const admin = require("firebase-admin");
 admin.initializeApp();
 
 function DateToUrlFriendlyString(date) {
-  return date.getDate() + "_" + date.getMonth() + 1 + "_" + date.getFullYear().toString();
+  return (
+    date.getDate() +
+    "_" +
+    date.getMonth() +
+    1 +
+    "_" +
+    date.getFullYear().toString()
+  );
 }
 
 exports.write = functions.https.onRequest((req, res) => {
+  res.set('Access-Control-Allow-Origin', '*');
   var speiseplan = {};
   speiseplan[DateToUrlFriendlyString(req.body.date)] = req.body.food;
-  return admin.database().ref("speiseplan").set(speiseplan).then(() => res.sendStatus(200));
+  return admin
+    .database()
+    .ref("speiseplan")
+    .set(speiseplan)
+    .then(() => res.sendStatus(200));
 });
 
 exports.read = functions.https.onRequest((req, res) => {
-  return admin.database().ref("speiseplan/" + req.query.date).on("value", snapshot => {
-    res.json(snapshot.val());
-  });
+  res.set('Access-Control-Allow-Origin', '*');
+  return admin
+    .database()
+    .ref("speiseplan/" + req.query.date)
+    .on("value", snapshot => {
+      res.json(snapshot.val());
+    });
 });
